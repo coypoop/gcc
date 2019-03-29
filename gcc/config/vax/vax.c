64028adadf8fbfa93a65b3bdf44385c4dd779d5b
coypu <coypu@sdf.org>
2019-03-29 20:49:47 +0300

    restore some type stuff and HImode/SImode/etc. changes
    Add the same headers as something else uses XXX most not needed

--- gcc/config/vax/vax.c
+++ gcc/config/vax/vax.c
@@ -24,18 +24,36 @@ along with GCC; see the file COPYING3.  If not see
 #include "target.h"
 #include "rtl.h"
 #include "tree.h"
+#include "gimple-expr.h"
 #include "df.h"
 #include "memmodel.h"
 #include "tm_p.h"
+#include "stringpool.h"
+#include "attribs.h"
+#include "expmed.h"
 #include "optabs.h"
 #include "regs.h"
 #include "emit-rtl.h"
+#include "recog.h"
+#include "diagnostic-core.h"
+#include "alias.h"
+#include "flags.h"
+#include "fold-const.h"
+#include "stor-layout.h"
 #include "calls.h"
 #include "varasm.h"
 #include "conditions.h"
 #include "output.h"
+#include "insn-attr.h"
+#include "explow.h"
 #include "expr.h"
+#include "gimplify.h"
+#include "langhooks.h"
 #include "reload.h"
+#include "tm-constrs.h"
+#include "params.h"
+#include "tree-pass.h"
+#include "context.h"
 #include "builtins.h"
 
 /* This file should be included last.  */
@@ -57,7 +75,7 @@ static void vax_function_arg_advance (cumulative_args_t, machine_mode,
 static rtx vax_struct_value_rtx (tree, int);
 static void vax_asm_trampoline_template (FILE *);
 static void vax_trampoline_init (rtx, tree, rtx);
-static int vax_return_pops_args (tree, tree, int);
+static poly_int64 vax_return_pops_args (tree, tree, poly_int64);
 static bool vax_mode_dependent_address_p (const_rtx, addr_space_t);
 static HOST_WIDE_INT vax_starting_frame_offset (void);
 /* Initialize the GCC target structure.  */
@@ -858,18 +876,18 @@ vax_rtx_costs (rtx x, machine_mode mode, int outer_code,
     case MULT:
       switch (mode)
 	{
-	case DFmode:
+	case E_DFmode:
 	  *total = 16;		/* 4 on VAX 9000 */
 	  break;
-	case SFmode:
+	case E_SFmode:
 	  *total = 9;		/* 4 on VAX 9000, 12 on VAX 2 */
 	  break;
-	case DImode:
+	case E_DImode:
 	  *total = 16;		/* 6 on VAX 9000, 28 on VAX 2 */
 	  break;
-	case SImode:
-	case HImode:
-	case QImode:
+	case E_SImode:
+	case E_HImode:
+	case E_QImode:
 	  *total = 10;		/* 3-4 on VAX 9000, 20-28 on VAX 2 */
 	  break;
 	default:
@@ -1179,7 +1197,7 @@ vax_output_int_move (rtx insn ATTRIBUTE_UNUSED, rtx *operands,
 
   switch (mode)
     {
-    case DImode:
+    case E_DImode:
       if (operands[1] == const0_rtx)
 	return "clrq %0";
       if (TARGET_QMATH && optimize_size
@@ -1292,7 +1310,7 @@ vax_output_int_move (rtx insn ATTRIBUTE_UNUSED, rtx *operands,
 	}
       return "movq %1,%0";
 
-    case SImode:
+    case E_SImode:
       if (symbolic_operand (operands[1], SImode))
 	{
 	  if (push_operand (operands[0], SImode))
@@ -1335,7 +1353,7 @@ vax_output_int_move (rtx insn ATTRIBUTE_UNUSED, rtx *operands,
 	return "pushl %1";
       return "movl %1,%0";
 
-    case HImode:
+    case E_HImode:
       if (CONST_INT_P (operands[1]))
 	{
 	  HOST_WIDE_INT i = INTVAL (operands[1]);
@@ -1352,7 +1370,7 @@ vax_output_int_move (rtx insn ATTRIBUTE_UNUSED, rtx *operands,
 	}
       return "movw %1,%0";
 
-    case QImode:
+    case E_QImode:
       if (CONST_INT_P (operands[1]))
 	{
 	  HOST_WIDE_INT i = INTVAL (operands[1]);
@@ -1383,11 +1401,11 @@ vax_output_int_move (rtx insn ATTRIBUTE_UNUSED, rtx *operands,
    which are not modified very often.  */
 
 const char *
-vax_output_int_add (rtx insn, rtx *operands, machine_mode mode)
+vax_output_int_add (rtx_insn *insn, rtx *operands, machine_mode mode)
 {
   switch (mode)
     {
-    case DImode:
+    case E_DImode:
       {
 	rtx low[3];
 	const char *pattern;
@@ -1473,7 +1491,7 @@ vax_output_int_add (rtx insn, rtx *operands, machine_mode mode)
 	return "adwc %2,%0";
       }
 
-    case SImode:
+    case E_SImode:
       if (rtx_equal_p (operands[0], operands[1]))
 	{
 	  if (operands[2] == const1_rtx)
@@ -1549,7 +1567,7 @@ vax_output_int_add (rtx insn, rtx *operands, machine_mode mode)
 
       return "addl3 %1,%2,%0";
 
-    case HImode:
+    case E_HImode:
       if (rtx_equal_p (operands[0], operands[1]))
 	{
 	  if (operands[2] == const1_rtx)
@@ -1568,7 +1586,7 @@ vax_output_int_add (rtx insn, rtx *operands, machine_mode mode)
 	return "subw3 $%n2,%1,%0";
       return "addw3 %1,%2,%0";
 
-    case QImode:
+    case E_QImode:
       if (rtx_equal_p (operands[0], operands[1]))
 	{
 	  if (operands[2] == const1_rtx)
@@ -1593,11 +1611,11 @@ vax_output_int_add (rtx insn, rtx *operands, machine_mode mode)
 }
 
 const char *
-vax_output_int_subtract (rtx insn, rtx *operands, machine_mode mode)
+vax_output_int_subtract (rtx_insn *insn, rtx *operands, machine_mode mode)
 {
   switch (mode)
     {
-    case DImode:
+    case E_DImode:
       {
 	rtx low[3];
 	const char *pattern;
@@ -2290,11 +2308,11 @@ vax_trampoline_init (rtx m_tramp, tree fndecl, rtx cxt)
 
    On the VAX, the RET insn pops a maximum of 255 args for any function.  */
 
-static int
+static poly_int64
 vax_return_pops_args (tree fundecl ATTRIBUTE_UNUSED,
-		      tree funtype ATTRIBUTE_UNUSED, int size)
+		      tree funtype ATTRIBUTE_UNUSED, poly_int64 size)
 {
-  return size > 255 * 4 ? 0 : size;
+  return size > 255 * 4 ? 0 : (HOST_WIDE_INT) size;
 }
 
 /* Define where to put the arguments to a function.
